4470cce063768bcdaca9abcd523b231e6d9373f0
tyknkd <78797823+tyknkd@users.noreply.github.com>
2022-08-01 07:22:32 +0900

    make postgres

--- wordsalad/schema.sql
+++ wordsalad/schema.sql
@@ -1,17 +1,17 @@
 CREATE TABLE users (
-  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL
+  user_id SERIAL PRIMARY KEY,
+  name VARCHAR ( 50 ) NOT NULL,
+  email VARCHAR ( 50 ) NOT NULL,
+  username VARCHAR ( 50 ) UNIQUE NOT NULL,
+  password VARCHAR ( 50 ) NOT NULL
 );
 
 CREATE TABLE decks (
-  deck_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  owner_id INTEGER NOT NULL, 
-  title TEXT NOT NULL,
-  category TEXT NOT NULL,
-  description TEXT,
+  deck_id SERIAL PRIMARY KEY,
+  owner_id INT NOT NULL, 
+  title VARCHAR ( 100 ) NOT NULL,
+  category VARCHAR ( 100 ) NOT NULL,
+  description VARCHAR ( 255 ),
   public BOOLEAN NOT NULL, 
   FOREIGN KEY (owner_id) 
     REFERENCES users (user_id)
@@ -20,11 +20,11 @@ CREATE TABLE decks (
 );
 
 CREATE TABLE cards (
-  card_id INTEGER PRIMARY KEY AUTOINCREMENT,
-  deck_id INTEGER NOT NULL, 
-  front TEXT NOT NULL,
-  back TEXT NOT NULL,
-  notes TEXT,
+  card_id SERIAL PRIMARY KEY,
+  deck_id INT NOT NULL, 
+  front VARCHAR ( 255 ) NOT NULL,
+  back VARCHAR ( 255 ) NOT NULL,
+  notes VARCHAR ( 255 ),
   FOREIGN KEY (deck_id) 
     REFERENCES decks (deck_id)
       ON DELETE CASCADE
